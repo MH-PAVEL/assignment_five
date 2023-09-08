acf33843ca5395497493e9a9672617731d87ee95
@mh-pavel <hasanmahamudulpavel786@gmail.com>
2023-09-08 23:19:02 +0600

    done

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text(
+          'Profile',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+            letterSpacing: 1.5,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.account_circle,
+              color: Colors.green,
+              size: 100.0,
+            ),
+            Text(
+              'Jhon Doe',
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.normal,
+                color: Colors.green,
+              ),
+            ),
+            Text(
+              'Flutter Batch 4',
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.blueAccent,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
